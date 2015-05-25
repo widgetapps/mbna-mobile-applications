72fd7d5c1edd45ec961d86283ebd5649fcd4bce1
Darryl Patterson <widgetapps@gmail.com>
2015-05-25 09:25:52 -0400

    And now no Sass/Compass.

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mbnaApplicationApp
+ * @description
+ * # mbnaApplicationApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mbnaApplicationApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mbnaApplicationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mbnaApplicationApp
+ */
+angular.module('mbnaApplicationApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
